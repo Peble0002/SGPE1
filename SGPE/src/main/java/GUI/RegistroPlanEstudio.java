308578445175dfcac696b4a12ba8f1a2d6600818
AaronSotco <65744689+AaronSotco@users.noreply.github.com>
2021-10-19 23:54:50 -0600

    Más cambios

--- SGPE/src/main/java/GUI/RegistroPlanEstudio.java
+++ SGPE/src/main/java/GUI/RegistroPlanEstudio.java
@@ -46,33 +46,29 @@ public class RegistroPlanEstudio extends javax.swing.JFrame {
   // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
   private void initComponents() {
 
-    jLabel5 = new javax.swing.JLabel();
     jLabel6 = new javax.swing.JLabel();
     cbEscuelaPropietaria = new javax.swing.JComboBox<>();
     btnRegistrarPlanEstudios = new javax.swing.JButton();
-    tbBloque = new javax.swing.JComboBox<>();
     btnRegresar = new javax.swing.JButton();
     jLabel1 = new javax.swing.JLabel();
     jLabel7 = new javax.swing.JLabel();
     jLabel2 = new javax.swing.JLabel();
     jLabel4 = new javax.swing.JLabel();
-    tbCodigoCursoPartePlan = new javax.swing.JTextField();
     tbCodigoCurso = new javax.swing.JTextField();
     tbVigenciaPlan = new javax.swing.JTextField();
-    jLabel8 = new javax.swing.JLabel();
 
     setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-    jLabel5.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-    jLabel5.setText("Código del curso que forma parte del plan:");
 
     jLabel6.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
     jLabel6.setText("Vigencia del plan de estudios: ");
 
     btnRegistrarPlanEstudios.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-    btnRegistrarPlanEstudios.setText("Registrar curso al plan de estudios");
-
-    tbBloque.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+    btnRegistrarPlanEstudios.setText("Registrar nuevo plan de estudios");
+    btnRegistrarPlanEstudios.addActionListener(new java.awt.event.ActionListener() {
+      public void actionPerformed(java.awt.event.ActionEvent evt) {
+        btnRegistrarPlanEstudiosActionPerformed(evt);
+      }
+    });
 
     btnRegresar.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
     btnRegresar.setText("Regresar");
@@ -93,12 +89,6 @@ public class RegistroPlanEstudio extends javax.swing.JFrame {
     jLabel4.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
     jLabel4.setText("Código del plan de estudio: ");
 
-    tbCodigoCursoPartePlan.addActionListener(new java.awt.event.ActionListener() {
-      public void actionPerformed(java.awt.event.ActionEvent evt) {
-        tbCodigoCursoPartePlanActionPerformed(evt);
-      }
-    });
-
     tbCodigoCurso.addActionListener(new java.awt.event.ActionListener() {
       public void actionPerformed(java.awt.event.ActionEvent evt) {
         tbCodigoCursoActionPerformed(evt);
@@ -112,31 +102,15 @@ public class RegistroPlanEstudio extends javax.swing.JFrame {
       }
     });
 
-    jLabel8.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-    jLabel8.setText("Bloque");
-
     javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
     getContentPane().setLayout(layout);
     layout.setHorizontalGroup(
       layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-      .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-        .addGap(0, 0, Short.MAX_VALUE)
-        .addComponent(jLabel8)
-        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-        .addComponent(jLabel7)
-        .addGap(123, 123, 123))
-      .addGroup(layout.createSequentialGroup()
-        .addGap(144, 144, 144)
-        .addComponent(tbCodigoCursoPartePlan, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
-        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        .addComponent(tbBloque, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-        .addGap(106, 106, 106))
       .addGroup(layout.createSequentialGroup()
         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
           .addGroup(layout.createSequentialGroup()
             .addGap(30, 30, 30)
             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-              .addComponent(jLabel5)
               .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                   .addComponent(jLabel6)
@@ -154,14 +128,16 @@ public class RegistroPlanEstudio extends javax.swing.JFrame {
             .addComponent(jLabel1)))
         .addContainerGap(198, Short.MAX_VALUE))
       .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        .addGap(0, 0, Short.MAX_VALUE)
         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
           .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-            .addComponent(btnRegresar)
-            .addGap(24, 24, 24))
+            .addComponent(jLabel7)
+            .addGap(123, 123, 123))
           .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
             .addComponent(btnRegistrarPlanEstudios)
-            .addGap(227, 227, 227))))
+            .addGap(101, 101, 101)
+            .addComponent(btnRegresar)
+            .addGap(30, 30, 30))))
     );
     layout.setVerticalGroup(
       layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -172,36 +148,25 @@ public class RegistroPlanEstudio extends javax.swing.JFrame {
         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
           .addComponent(jLabel2)
           .addComponent(cbEscuelaPropietaria, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-        .addGap(28, 28, 28)
+        .addGap(39, 39, 39)
         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
           .addComponent(jLabel4)
           .addComponent(tbCodigoCurso, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-        .addGap(33, 33, 33)
+        .addGap(43, 43, 43)
         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
           .addComponent(jLabel6)
           .addComponent(tbVigenciaPlan, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-        .addGap(39, 39, 39)
+        .addGap(41, 41, 41)
         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-          .addComponent(jLabel7)
-          .addComponent(jLabel5)
-          .addComponent(jLabel8))
-        .addGap(33, 33, 33)
-        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-          .addComponent(tbCodigoCursoPartePlan, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-          .addComponent(tbBloque, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 52, Short.MAX_VALUE)
-        .addComponent(btnRegistrarPlanEstudios)
-        .addGap(18, 18, 18)
-        .addComponent(btnRegresar)
-        .addContainerGap())
+          .addComponent(btnRegistrarPlanEstudios)
+          .addComponent(btnRegresar))
+        .addGap(26, 26, 26)
+        .addComponent(jLabel7)
+        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
     );
 
     pack();
   }// </editor-fold>//GEN-END:initComponents
-
-    private void tbCodigoCursoPartePlanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tbCodigoCursoPartePlanActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_tbCodigoCursoPartePlanActionPerformed
 
     private void tbCodigoCursoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tbCodigoCursoActionPerformed
         // TODO add your handling code here:
@@ -217,6 +182,10 @@ public class RegistroPlanEstudio extends javax.swing.JFrame {
       
       this.dispose();
     }//GEN-LAST:event_btnRegresarActionPerformed
+
+  private void btnRegistrarPlanEstudiosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistrarPlanEstudiosActionPerformed
+    // TODO add your handling code here:
+  }//GEN-LAST:event_btnRegistrarPlanEstudiosActionPerformed
 
     /**
      * @param args the command line arguments
@@ -260,13 +229,9 @@ public class RegistroPlanEstudio extends javax.swing.JFrame {
   private javax.swing.JLabel jLabel1;
   private javax.swing.JLabel jLabel2;
   private javax.swing.JLabel jLabel4;
-  private javax.swing.JLabel jLabel5;
   private javax.swing.JLabel jLabel6;
   private javax.swing.JLabel jLabel7;
-  private javax.swing.JLabel jLabel8;
-  private javax.swing.JComboBox<String> tbBloque;
   private javax.swing.JTextField tbCodigoCurso;
-  private javax.swing.JTextField tbCodigoCursoPartePlan;
   private javax.swing.JTextField tbVigenciaPlan;
   // End of variables declaration//GEN-END:variables
 }
